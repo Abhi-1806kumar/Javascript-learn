c564a786e2e57fbc9d71bd9e7fc6b9cecd8ce89c
abhishek <abhsihekkumarbhagat18@gmali.com>
2023-07-25 20:03:44 +0530

    done

---
+++ 1_js.js
@@ -0,0 +1,16 @@
+// it is about the const,var,let...
+
+const account = 1234
+let account_name = "abhi"
+var account_user="abhi007"
+Account_city="BNG"
+  // we dont use var it prefer to use the let and const bcz var have the scope porblem 
+
+  console.log([account,account_name,account_user,Account_city])
+
+
+account_name="vlog"
+account_user="abhi_vlogs"
+Account_city="CHE"
+console.log([account,account_name,account_user,Account_city])
+
---
+++ 2_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// treat all js code as newer version
+
+// tpyes of data types
+
+let name = "abhii"
+let age  = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//string=>""
+//boolean=>true or false
+// null=>standalone value 
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof false);
