ef253ac24e47287a3a56551dbd1e8679f6d697e9
abhishek <abhsihekkumarbhagat18@gmali.com>
2023-07-28 09:51:52 +0530

    done

---
+++ 3_dataconversion.js
@@ -0,0 +1,49 @@
+let views=176
+console.log(typeof views);
+
+let view="176"
+console.log(typeof view);
+
+let vieew="179adcc"
+
+let valueInNumber = Number(vieew)
+console.log(typeof valueInNumber);
+console.log(valueInNumber) 
+// it give NaN means it has issue with the conversion like this
+//===============================================================//
+// bolean conversion 
+
+let IsloggedIn = 1
+
+let IsBoolean = Boolean(IsloggedIn)
+console.log(IsBoolean);
+console.log(typeof IsBoolean);
+
+// String   Conversion
+
+let Number=33
+let StringNumber = String(Number)
+console.log(typeof StringNumber);
+console.log(StringNumber);
+
+//=============================================================//
+//********** OPERATIONS ************************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//  Basic operation
+
+// => +,-,/,*,**,%
+
+let str1 = "Abhi"
+let str2 = "Vlogs"
+
+console.log(str1+" "+str2);
+
+
+
+
+
+
---
+++ 4_comparsion.js
@@ -0,0 +1,21 @@
+console.log(2<4);
+// simialry we have many basic comparsion same in string also 
+// <=,=>,... so on 
+
+console.log("2">3);
+// camparing string and number so it wil  convert the string to the number 
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0)
+console.log(undefined > 0 )
+console.log(undefined < 0)
+
+// here the "==" and "comparsion operator " have different working
+
+
+// "===" it is use for strict use
+console.log("2"=== 2);
