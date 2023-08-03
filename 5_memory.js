9983569b40615935a1d2d03e2405cde93e346fbb
abhishek <abhsihekkumarbhagat18@gmali.com>
2023-08-03 20:27:06 +0530

    done

---
+++ 5_memory.js
@@ -0,0 +1,23 @@
+//===================
+// STACK(Primative)   , HEAP(Non-Primative or reference)
+
+let myyoutube="Abhi_vlogs"
+
+let youtube=myyoutube
+
+ youtube="Abhi_Kumar"
+
+ console.log(myyoutube);
+ console.log(youtube);
+
+ let userone={
+    email:"Abhishek@gmail.com",
+    name:"Abhi"
+
+ }
+
+ let usertwo=userone
+
+ usertwo.email="Abhi_Vlogs"
+ console.log(userone.email);
+ console.log(usertwo.email);
---
+++ 6_String.js
@@ -0,0 +1,16 @@
+const name = "Abhiii"
+const repocount = 50
+
+//console.log(name+repocount+"value")
+// THis is not a good pratice 
+
+console.log(`hello my name is ${name} and my repo count is ${repocount} `);
+
+//************************************************************//
+
+//Different Ways to initalize the string 
+
+const gamename = new String('ABHIIvlogs')
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toLocaleLowerCase())
