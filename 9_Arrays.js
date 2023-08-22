c46ba29b37e03e88f0d72d3307e5667a9ec60a05
abhishek <abhsihekkumarbhagat18@gmali.com>
2023-08-22 15:27:16 +0530

    Done

---
+++ 9_Arrays.js
@@ -0,0 +1,32 @@
+// Arrays 
+
+const myarr = [0,1,2,4,5]
+const myarr1 = ["Abhi","vlogs"]
+
+console.log(myarr[1])
+console.log(myarr)
+console.log(myarr1)
+
+// methods 
+
+myarr.push(6)
+myarr.push(7)
+myarr.pop()
+console.log(myarr)
+
+// ther r also shift and unshift
+// there r slice & splice
+
+const myn1 = myarr.slice(1,3)
+console.log(myn1)
+const myn2 = myarr.splice(1,3)
+console.log(myn2)
+
+// Arrays other 
+
+const marvel =["abhi","Vlogs","kuch","nhi"]
+const heros=["welcome","to","friends","hello","vlog"]
+
+const allhero=marvel.concat(heros)
+console.log(allhero)
+
